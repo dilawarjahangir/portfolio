401ae078ed7ebf0e73ad5cd68975081765c6d93c
dilawarjahangir <dilawarjahangir2@gmail.com>
2025-02-11 15:40:06 +0500

    first-commit

---
+++ _next/static/chunks/app/blog/[id]/page-96b81c207510bbb4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[548],{225:function(e,n,s){Promise.resolve().then(s.bind(s,4751)),Promise.resolve().then(s.t.bind(s,5250,23))}},function(e){e.O(0,[647,628,751,971,69,744],function(){return e(e.s=225)}),_N_E=e.O()}]);
